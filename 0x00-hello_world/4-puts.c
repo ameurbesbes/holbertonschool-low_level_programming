6034a7d597300fd3d22b641920c9bec0f3952bff
ameurbesbes <ameurbesbes@outlook.com>
2021-02-08 12:58:56 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * print message 
+ * print message
  * Return: Sucess
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
