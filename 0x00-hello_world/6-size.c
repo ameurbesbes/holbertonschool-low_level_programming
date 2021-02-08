01dadd4ecc2447de02589061d9b22cc3ef28db03
ameurbesbes <ameurbesbes@outlook.com>
2021-02-08 14:54:04 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- * print size of var types
- * Return: Sucess
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,6 +10,7 @@ printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("size of a float: %d byte(s)\n", sizeof(float));
+printf("size of a float: %d byte(s)\n", sizeof(float))
+
 return (0);
 }
