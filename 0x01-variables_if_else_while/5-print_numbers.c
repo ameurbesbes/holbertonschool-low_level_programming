39dd72e7429113047848cb5e6b2091dc58f1d209
ameurbesbes <ameurbesbes@outlook.com>
2021-02-11 00:11:21 +0100

    5 print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * display message new way
+ * Return: Sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
