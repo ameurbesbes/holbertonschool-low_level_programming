ea1068b7688e70893fa19a5f622c4f8d6dcc7b04
ameurbesbes <ameurbesbes@outlook.com>
2021-02-11 15:41:26 +0100

    2-print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+	_putchar('\n');
+	}
+	return (0);
+}
