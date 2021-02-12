88005a38209e1fb52bcb2824f6f875c8fa1457a6
ameurbesbes <ameurbesbes@outlook.com>
2021-02-12 09:36:24 +0100

    3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _islower - return if lower
+ * @c : ascii to check
+ * Return: int
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+			return (1);
+	else
+		return (0);
+}
