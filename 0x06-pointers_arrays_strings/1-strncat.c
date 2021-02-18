31b05a51d1dacc33a2c2a8d7018fe43cbf8e6e72
ameurbesbes <ameurbesbes@outlook.com>
2021-02-18 15:38:06 +0100

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncat - concat
+ *@dest: dest char*
+ *@src: source char*
+ *@n: integer
+ * Return: new concanated string
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && source[j] != '\0')
+	{
+		dest[i] = source[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
