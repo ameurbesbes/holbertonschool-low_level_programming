2f7b51016f15feb8c303f940eddfb620723ce01f
ameurbesbes <ameurbesbes@outlook.com>
2021-02-23 15:37:33 +0100

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,17 @@
 #include"holberton.h"
+#include <stdio.h>
 /**
  * _strchr - finds if char occurs in string
  * @s: string to check
  * @c: character to find in s
- * Return: pointer to first instance of c, otherwise NULL
+ * Return: NULL
 */
 char *_strchr(char *s, char c)
 {
 	while (s++)
-{
-	if ((*s) == c)
-	return (s);
-}
-	return (0);
+	{
+		if ((*s) == c)
+		return (s);
+	}
+	return (NULL);
 }
