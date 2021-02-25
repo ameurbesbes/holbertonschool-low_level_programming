d3a004da0ddb5e8db5b6f76c3b8d6f63fc2c35e1
ameurbesbes <ameurbesbes@outlook.com>
2021-02-25 15:23:12 +0100

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include"holberton.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: pointer
+ * Return: length
+*/
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+if (*s != '\0')
+	{
+		len++;
+		len = len + _strlen_recursion(s+1);
+	}
+	return (len);
+
+}
