3c5cce1f6394be35a3267d1018db5f394c22f336
ameurbesbes <ameurbesbes@outlook.com>
2021-02-25 13:48:51 +0100

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include "holberton.h"
 /**
- * 
- * 
- * 
- * 
- * 
+ * _print_rev_recursion - function reverse a string.
+ * @s:string
+ *
+ * Return: no return
  */
 void _print_rev_recursion(char *s)
 {
-        if (*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-                _putchar(*s);
-
+		_putchar(*s);
 	}
 }
