6057d2cf183e2f2257c9b47427fdb5a97f30a0b1
ameurbesbes <ameurbesbes@outlook.com>
2021-02-25 15:27:01 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,10 +7,11 @@
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-if (*s != '\0')
+
+	if (*s != '\0')
 	{
 		len++;
-		len = len + _strlen_recursion(s+1);
+		len = len + _strlen_recursion(s + 1);
 	}
 	return (len);
 
