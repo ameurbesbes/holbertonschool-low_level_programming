9b0c8d1fd8bfc72c37a2eae495cf4666dd4174bc
ameurbesbes <ameurbesbes@outlook.com>
2021-02-26 23:54:00 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*print_line -0
+*@n:int
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
