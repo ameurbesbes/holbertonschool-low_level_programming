f57895048ddfdd25c99d087494b3a1627edf4e36
ameurbesbes <ameurbesbes@outlook.com>
2021-02-26 23:17:10 +0100

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-*_isupper -0
+*_isdigit -0
 *@c: char
 *Return: return 1 or 0.
 */
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*mul -0
+*@a: int
+*@b: int
+*Return: return 1 or 0.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
