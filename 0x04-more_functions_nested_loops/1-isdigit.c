7b960388801dc3c9dcf88d975c77b3180ffc4c3e
ameurbesbes <ameurbesbes@outlook.com>
2021-02-26 23:09:39 +0100

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_isupper -0
+*@c: char
+*Return: return 1 or 0.
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
