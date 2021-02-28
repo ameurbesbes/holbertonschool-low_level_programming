eca2fbba36a1f9ace71f0bc8d91f076173042b5d
ameurbesbes <ameurbesbes@outlook.com>
2021-02-28 16:01:13 +0100

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz test program
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+int b;
+for (b = 1; b <= 100; b++)
+{
+if ((b % 3 == 0) && (b % 5 == 0))
+printf("FizzBuzz");
+else if (b % 3 == 0)
+printf("Fizz");
+else if (b % 5 == 0)
+printf("Buzz");
+else
+printf("%i", b);
+if (b != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
