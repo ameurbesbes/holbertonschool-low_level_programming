b2cf1dd3b94051b5bfebeb4a91c9f47faf7a5e85
ameurbesbes <ameurbesbes@outlook.com>
2021-03-01 16:03:12 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,22 +7,29 @@
  * @argv : pointer
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int sum = 0, i;
-if (argc == 1)
-{
-printf("%d\n", 0);
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && *argv[i] != '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
 return (0);
-}
-for (i = 1; i < argc; i++)
-{
-if (atoi(argv[i]) == 0)
-{
-printf("Error\n");
-return (1);
-}
-sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
 }
