166bbe7b76107e04667b74e166c95595798ffccf
ameurbesbes <ameurbesbes@outlook.com>
2021-03-01 14:34:19 +0100

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print argc - 1
+ * @argc : int
+ * @argv : pointer
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
