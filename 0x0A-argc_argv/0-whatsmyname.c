03919afb7cdb2930bfd1dd43b8cceb4e51a7fdd6
ameurbesbes <ameurbesbes@outlook.com>
2021-03-01 14:17:53 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print name
+ * @argc : int
+ * @argv : pointer
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
