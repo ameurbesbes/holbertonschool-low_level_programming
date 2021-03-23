fe890a0a4d56dc0b2620012a5c1f94982035252e
ameurbesbes <ameurbesbes@outlook.com>
2021-03-23 13:26:32 +0100

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint - print items in a list
+ * @h: pointer
+ * Return: number of elements
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0, s = 0;
+
+	if (h == NULL)
+		return (x);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		s++;
+		h = h->next;
+	}
+	return (s);
+}
