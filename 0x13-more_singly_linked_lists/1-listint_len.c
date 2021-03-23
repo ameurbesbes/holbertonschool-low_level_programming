f092cf3de0f18004615d3cf55ca3cc26bffb725a
ameurbesbes <ameurbesbes@outlook.com>
2021-03-23 22:55:19 +0100

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - print items in a list
+ * @h: pointer
+ * Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t x;
+
+	if (h == NULL)
+		return (0);
+	for (x = 0; h != NULL; x++)
+	{
+		h = h->next;
+	}
+	return (x);
+}
