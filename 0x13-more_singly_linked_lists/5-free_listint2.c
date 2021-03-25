eea593b672c841421b9301fb7c0a9b1fa02e4712
ameurbesbes <ameurbesbes@outlook.com>
2021-03-25 11:04:14 +0100

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_listint2 - function that frees listint_t
+ * @head: pointer
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *temp, *new;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	new = *head;
+	while (new != NULL)
+	{
+	temp = new;
+	new = new->next;
+	free(temp);
+	}
+	*head = NULL;
+}
